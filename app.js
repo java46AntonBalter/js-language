1ac4f4942de031349904a0b097403aa9dd928b55
Anton Balter <antbalter@gmail.com>
2022-01-15 17:41:17 +0200

    hw#11

--- app.js
+++ app.js
@@ -1,52 +1,37 @@
-/*************************Strings */
-const str1 = "abc";
-const str2 = "abc";
-console.log(`str1 == str2 is ${str1 == str2}`)
-console.log(`"abc" < "ab" is ${str1 < "ab"}`)
-console.log(`"123" > 23 is ${"123" > 23}`);
-console.log(`"123" > "23" is ${"123" > "23"}`);
-/****************************************** */
-//if  logical expression contains both string and number, JS will convert string to number
-//if string doesn't contain a number a result of conversion will be NaN
-//In any logical expression if there is NaN, then the result will be false
-console.log(`"abc" > 23 is ${"abc" > 23}`);
-console.log(`"abc" < 23 is ${"abc" < 23}`);
-console.log(`"abc" != 23 is ${!("abc" == 23)}`)
-/********************************************* */
-function stringProcessing(str) {
-        const strP = "" + str;
-       console.log(`"${str}"[4] is ${str[4]}`);
-       console.log(`length of "${str}" is ${strP.length}`);
-       console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
-       console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
-       console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")}; last index of substring "ll" in the "${str}" is  ${strP.lastIndexOf("ll")}`)
+const codingString = 'qwertyuiopasdfghjklzxcvbnm';
+const num = 466600005555;
+console.log(`base is: ${codingString.length}`);
+const dupeCheckRes = charDupe(codingString);
+console.log(`Dupe Check: ${dupeCheckRes}`);
 
+if(dupeCheckRes == true){
+    console.log('ERROR: There are duplicate symbols in the codingString!!!');
 }
-//stringProcessing("abcd*lmn*ll(oo&&ttll");
-/********************HW 11 definition */
-// function encode(num, base) {
-//         //base from 2 to 10
-//         let res="";
-//         do {
-//                 const digit = Math.trunc(num % base);
-//                 const symb = getSymbol(digit);
-//                 res = symb + res;
-//                 num = Math.trunc(num / base);
+else if(dupeCheckRes == false){
+    console.log(encode(num, codingString));
+}
 
-//         } while(num >= 1);
-//         return res;
-// }
-// function getSymbol(digit) {
-//       //base from 2 to 10 
-//       console.log(digit);
-//       return "" + digit; //it will work only for base <= 10 
-// }
-console.log(encode(10, 2))
-// write function 
-function encode (num, codingString) {
-        //codingString - any string with no repeated symbols
-        // you should validate codingString doesn't contain repeated symbols
-        //base = length of codingString
-        // algorithm the same as specified above
-        // getSymbol(digit, codingString) using operator []
+function charDupe(str) {
+    for(let i = 0; i<str.length; i++) {
+        if(str.indexOf(str[i]) != str.lastIndexOf(str[i])){
+            return true;
+        }
+    }
+    return false
+}
+
+function encode(num, str) {
+    let res = "";
+    do  {
+        let base = str.length;
+        const digit = Math.trunc(num % base);
+        const symb = getSymbol(digit);
+        res = symb + res;
+        num = Math.trunc(num / base);
+    } while (num >= 1);
+    return res;
+}
+   
+function getSymbol(digit) {
+    return "" + codingString[digit]; 
 }
