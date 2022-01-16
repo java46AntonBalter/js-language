699b219931440e4c162e9dca7688c9bb9ac28494
Anton Balter <antbalter@gmail.com>
2022-01-16 23:17:54 +0200

    hw#11

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
-const codingString = 'qwertyuiopasdfghjklzxcvbnm';
-const num = 466600005555;
-console.log(`base is: ${codingString.length}`);
-const dupeCheckRes = charDupe(codingString);
-console.log(`Dupe Check: ${dupeCheckRes}`);
-
-if(dupeCheckRes == true){
-    console.log('ERROR: There are duplicate symbols in the codingString!!!');
-}
-else if(dupeCheckRes == false){
-    console.log(encode(num, codingString));
-}
-
-function charDupe(str) {
-    for(let i = 0; i<str.length; i++) {
-        if(str.indexOf(str[i]) != str.lastIndexOf(str[i])){
-            return true;
-        }
-    }
-    return false
-}
-
 function encode(num, str) {
     let res = "";
+    const base = str.length;
+    for(let i = 0; i<str.length; i++) {
+        if(i != str.lastIndexOf(str[i])){
+            console.log('ERROR: There are duplicate symbols in the codingString!!!');
+            return NaN;
+        }
+    }
     do  {
-        let base = str.length;
         const digit = Math.trunc(num % base);
-        const symb = getSymbol(digit);
-        res = symb + res;
+        res = codingString[digit] + res;
         num = Math.trunc(num / base);
-    } while (num >= 1);
+    } while (num > 0);
     return res;
 }
-   
-function getSymbol(digit) {
-    return "" + codingString[digit]; 
-}
+
+
+const codingString = ".-"
+console.log(encode(5, codingString));
