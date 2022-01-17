971c1a1b719fb5041f2f312a44af1f1c76fd195b
Anton Balter <antbalter@gmail.com>
2022-01-17 23:57:37 +0200

    hw#12

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-function encode(num, str) {
-    let res = "";
-    const base = str.length;
-    for(let i = 0; i<str.length; i++) {
-        if(i != str.lastIndexOf(str[i])){
-            console.log('ERROR: There are duplicate symbols in the codingString!!!');
-            return NaN;
+/************ HW #12 ********/
+/**** Comparator ****/
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+arHw.sort((a, b) => (a % 2) - (b % 2));
+arHw.sort(function(a,b){
+    if((a % 2 == 0) && (b % 2 == 0)){
+        return a-b;
+    }
+    else if((a % 2 != 0) && (b % 2 != 0)){
+        return b-a;
+    }
+})
+console.log(arHw.toString());
+/******************************************* */
+
+function matrixTransp(matrix){
+    const newMatrix = [];
+    for (let i = 0; i < 1; i++){
+        for (let j = 0; j < matrix[i].length; j++){
+            newMatrix.push([]);
         }
     }
-    do  {
-        const digit = Math.trunc(num % base);
-        res = codingString[digit] + res;
-        num = Math.trunc(num / base);
-    } while (num > 0);
-    return res;
+    for(let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            newMatrix[j].splice(200, 0, matrix[i][j])
+        }
+    }
+    console.log(newMatrix);
+    displayMatrix(newMatrix);
 }
 
-const codingString = ".-"
-console.log(encode(5, codingString));
+function displayMatrix(matrix){
+    for(let i = 0; i < matrix.length; i++) {
+        let row = '';
+        for (let j = 0; j < matrix[i].length; j++) {
+            row = row + matrix[i][j] + '  ';
+        }
+        console.log(row);
+    }
+}
+const matrix = [[1, 2], [3, 4], [5, 6]];
+console.log(matrix);
+displayMatrix(matrix);
+matrixTransp(matrix);
