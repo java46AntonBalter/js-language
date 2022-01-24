59ee958dcc6639ab09722e8db0ab2575b49609f5
Anton Balter <antbalter@gmail.com>
2022-01-25 00:28:28 +0200

    hw#14

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+/*****************Arrays */
 const ar = [-10, 50, -12, 80, 40, 70];
 
 const ar2 = myFilter(ar, (n, i ,a) => i % 2 == 0 ? n + a.length: n - a.length);
 console.log(ar2);
 
-const ar3 = myReduce(ar, (n, i, a) => (n >= i) && (n >= a.length) ? n : 0);
+
+const ar3 = myReduce(ar, (n, i, a) => (n >= i) && (n >= a.length) ? n : 0, 0);
 console.log(ar3);
-const ar4 = myReduce(ar, (n, i, a) => (n >= i) && (n >= a.length) ? n : 0, 0);
-console.log(ar3);
+const ar4 = myReduce(ar, (n, i, a) => (n >= i) && (n >= a.length) ? n : 0);
+console.log(ar4);
 
 function myForEach(array, callback) {
     for (let i = 0; i < array.length; i++) {
@@ -22,7 +24,7 @@ function myFilter(array,callback) {
     return res;
 }
 function myReduce(array, callback, initialResult){
-    if(initialResult !== undefined){
+    if(initialResult != undefined){
         let res = initialResult;
         function forEachCall(n, i, a) {
             res = res + callback(n, i, a);
@@ -40,3 +42,25 @@ function myReduce(array, callback, initialResult){
         return res;
     }
 }
+
+/******************Objects */
+function createAddress(city, street) {
+    return {city: city, street: street};
+}
+function createPerson(id, name, address) {
+    return{id, name, address};
+
+}
+const persons = [
+    createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
+    createPerson(124, "Olya", createAddress("Rehovot", "Pr. Plaut")),
+    createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
+]
+
+
+const res = persons.filter(n => n.address.city == "Rehovot");
+console.log(`People living in Rehovot are:`);
+console.log(res);
+
+persons.sort((a, b) => a.address.city != "Rehovot"? -1 : 1);
+console.log(persons);
