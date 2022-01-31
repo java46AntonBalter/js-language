95193112807d8521d3ec0d8ae21f6148931d61f0
Anton Balter <antbalter@gmail.com>
2022-01-31 18:13:59 +0200

    hw#15

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ function reducer(array){
 }
 function displayOccurrences(array) {
     const res = reducer(array);      
-    Object.entries(res).sort((e1, e2) => {
-        const res = e2[1] - e1[1];
-        return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
-    }).forEach(e => console.log(`${e[0]} -> ${e[1]}`));
+    Object.entries(res).sort((e1, e2) => e2[1] - e1[1] || e1[0].localeCompare(e2[0])).forEach(e => console.log(`${e[0]} -> ${e[1]}`));
 }
 const ar = ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
 displayOccurrences(ar);
