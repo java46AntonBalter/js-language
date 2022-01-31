1730be692636f9836568ec9f8b807478e5926dac
Anton Balter <antbalter@gmail.com>
2022-01-31 18:23:37 +0200

    hw#15 solution

--- app.js
+++ app.js
@@ -29,100 +29,121 @@
 //         createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
 //         createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
 // ]
-// /**************************************HW 14 definition task3 */ 
+/**************************************HW 14 definition task3 */ 
 
-// //TODO write function getPersonsCity(persons, city) , taking array of the persons and a city (see the branch js-objects ) and returning array of the persons living in the given city. For example, find persons living in Rehovot 
+//TODO write function getPersonsCity(persons, city) , taking array of the persons and a city (see the branch js-objects ) and returning array of the persons living in the given city. For example, find persons living in Rehovot 
 
-// // Input: const persons = [ 
+// Input: const persons = [ 
 
-// //         createPerson(123, "Vasya", createAddress("Rehovot","Parshani")), 
+//         createPerson(123, "Vasya", createAddress("Rehovot","Parshani")), 
 
-// //         createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")), 
+//         createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")), 
 
-// //         createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")), 
+//         createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")), 
 
-// //         createPerson(126, "Sara", createAddress('Lod', 'Sokolov')) 
+//         createPerson(126, "Sara", createAddress('Lod', 'Sokolov')) 
 
-// // ] 
+// ] 
 
-// // Output: getPersonsCity(persons, ‘Rehovot’) => [ 
+// Output: getPersonsCity(persons, ‘Rehovot’) => [ 
 
-// //         {id: 123, name: "Vasya", address:{city: "Rehovot",street: "Parshani")), 
+//         {id: 123, name: "Vasya", address:{city: "Rehovot",street: "Parshani")), 
 
-// //         {id: 124, name: "Olya", address:{city: "Rehovot",street: "Pr. Plaut")) 
+//         {id: 124, name: "Olya", address:{city: "Rehovot",street: "Pr. Plaut")) 
 
-// // ] 
+// ] 
 
  
  
 
-// /****************************************HW 14 definition task4 */ 
+/****************************************HW 14 definition task4 */ 
 
-// //TODO write function movePersonsNoCityAtBeginning(persons, city) taking array of the persons and a city (see the branch js-objects ) and returning new array with all persons not living in the given city moved at beginning; 
+//TODO write function movePersonsNoCityAtBeginning(persons, city) taking array of the persons and a city (see the branch js-objects ) and returning new array with all persons not living in the given city moved at beginning; 
 
-// // 
-// /************************************************ */
+// 
+/************************************************ */
 
-// /***************HW 14 solutions */
-// //task 3
+/*************************************************HW 14 solutions */
+//task 3
 // function getPersonsCity(persons, city) {
-//     return persons.filter(p => p.address.city === city);
+//         return persons.filter(p => p.address.city === city);
 // }
 // console.log(getPersonsCity(persons, 'Rehovot'));
-// //task4
+//task 4
 // function movePersonsNoCityAtBeginning(persons, city) {
-//     const personsRes = getPersonsCity(persons, city);
-//     personsRes.unshift(...persons.filter(p => p.address.city !== city));
-//     return personsRes;
+//         const personsRes = getPersonsCity(persons, city);
+//         personsRes.unshift(...persons.filter(p => p.address.city !== city));
+//         return personsRes;
 // }
-// console.log(movePersonsNoCityAtBeginning(persons, 'Rehovot'));
-/***************************cw #15 */
-//input:["lmn", "d", "d", "lmn", "a", "lmn", "a", "bc"]
+// console.log(movePersonsNoCityAtBeginning(persons,  'Rehovot'));
+/****************************cw #15 */
+//input: ["lmn", "d", "d", "lmn", "a", "lmn", "a", "bc"]
 //output: lmn -> 3
 //        a -> 2
 //        d -> 2
 //        bc -> 1
-function displayOccurences(array) {
-    //creating object with key - unique element of the array (string as an element of array)
-    //                     value - occurences count
-    //difference between obj = {a: 123, d: "abc"}; const a = "d" ; obj.a === 123,
+function displayOccurrences(array) {
+    //creating object with key - unique element of array (string as an element of array)
+    //                     value - occurrences count
+    //difference between  obj ={a: 123, d: "abc"}; const a = "d" ; obj.a === 123,
     // and obj[a] === "abc"
     //obj.c = 10 -> {a: 123, d: "abc", c: 10}
-    const res = {};
-    for (let i = 0; i < array.length; i++) {
-        if(res[array[i]] === undefined){
-            //string as content of array[i] occures first time
-            res[array[i]] = 1;
-        } else {
-            res[array[i]] = res[array[i]] + 1
-        }        
-    }
-    // console.log(res);
+    const res = getStatObj(array); ;
+   
+    // console.log(res) -> intermediate debug log
     Object.entries(res).sort((e1, e2) => {
-        const res = e2[1] - e1[1];
-        return res === 0 ? e1[0].localeCompare(e2[0]) : res;
-    }).forEach(e => console.log(`${e[0]} -> ${e[1]}`));
+            const res = e2[1] - e1[1];
+            return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
+    }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
+
+
 }
 const ar = ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
-displayOccurences(ar);
-/*************************************HW #15 task1 */
-//refactoring of displayOccurences function
-//lines 91-99 should be a seperated function
-//that separates function should apply standard methods like reduce
+displayOccurrences(ar);
 
-/****************************HW #15 task2 */
-// write useful function countBy(array, callbackFun) that returns an object with keys as grouping criteria and values as the occurrence counts
-//keys should be sorted (optional)
-//where array - any array, callbackFun - function returning grouping critera
-//example1:
-//const arr = [6.4, 7.3, 6.5, 6.9]
+function getStatObj(array) {
+   
+   return array.reduce((res,cur) => {
+            res[cur] = res[cur] === undefined ? 1 : res[cur] + 1;
+            return res;
+    }, {})
+}
+/**********************************************HW #15 task1 */
+//refactoring of displayOccurrences function from the classwork #15
+//lines 92 - 99 should be a separated function
+//that separated function should apply standard methods like reduce
+
+/**********************************************************HW #15 task2 */
+// write useful function countBy(array, callbackFun) that returns object
+// with keys as grouping criteria  and values as the occurrence counts
+
+//where array - any array, callbackFun - function returning grouping criteria
+//examples:
+// const arr = [6.4, 7.3, 6.5, 6.9];
 //const statistics = countBy(arr, element => Math.floor(element))
-//result: statistics -> {"6": 3, "7": 1}
-//example2:
+//result: statistics -> {"6": 3, "7":1}
 // const arr = ['abcd', 'lmnr', 'ab', 'dddd'];
 //const statistics = countBy(arr, element => element.length)
-//result: statistics -> {"4" : 3, "2" : 1}
-//example3:
-//const arr = [{age:25 , id: 123, name: Vasya},{age:50 , id: 123, name: Vasya},{age:25 , id: 123, name: Vasya},{age:70 , id: 123, name: Vasya}]
-//const statistics = countBy(arr, element => element.age)
+//result: statistics -> {"4": 3, "2":1}
+//const arr = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
+// {age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}  ]
+//const statistics = countBy(arr, element -> element.age)
 //result statistics -> {"25":2, "50":1, "70":1}
+function countBy(array, callbackFn) {
+    array = array.map(callbackFn);
+    return getStatObj(array);
+}
+let arr = [6.4, 7.3, 6.5, 6.9];
+let statistics = countBy(arr, element => Math.floor(element))
+//result: statistics -> {"6": 3, "7":1}
+console.log(statistics)
+arr = ['abcd', 'lmnr', 'ab', 'dddd'];
+statistics = countBy(arr, element => element.length)
+//result: statistics -> {"4": 3, "2":1}
+console.log(statistics)
+arr = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
+{age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}  ]
+statistics = countBy(arr, element => element.age)
+//result statistics -> {"25":2, "50":1, "70":1}
+console.log(statistics)
+
