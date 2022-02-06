d02aa4a50ae66b040d102549e00fd44cfcd360e8
Anton Balter <antbalter@gmail.com>
2022-02-06 22:19:28 +0200

    HW#15 - extra

--- app.js
+++ app.js
@@ -1,48 +1,52 @@
-/*******************************************************HW #16 task 1 */
 
-const Deferred = function() {
-    this.accArray = [];
-    this.res;
+ function createAddress (city, street) {
+    return {city, street}
 }
-Deferred.prototype.then = function (newFunc) {
-    this.accArray.push(newFunc);
+function createPerson(id, name, address) {
+    return {id, name, address}
 }
-Deferred.prototype.resolve = function (value) {
-    this.res = value;
-    this.accArray.forEach(n => this.res = n(this.res));
-}
-    
-const d = new Deferred();
-d.then(function(res){ console.log('1 ', res); return 'a'; });
+const persons = [
+    createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
+    createPerson(124, "Olya",  createAddress("Rehovot", "Pr.Plaut")),
+    createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
+    createPerson(126, "Sara",  createAddress("Lod", "Sokolov"))
+] 
 
-d.then(function(res){ console.log('2 ', res); return 'b'; });
-
-d.then(function(res){ console.log('3 ', res); return 'c'; });
-d.resolve('hello');
-
-
-
-/*******************************************HW #16 task2 */
-function MyArray(value){
-    this.accArray = []; 
-    this.value = value;
+function groupBy(array, keyExtractor, groupInitializer, groupReducer) {
+    return array.reduce(
+        (rv, n) => {
+            let key = keyExtractor(n);
+            if (rv[key] === undefined) {
+                rv[key] = groupInitializer();
+            }
+            rv[key] = groupReducer(rv[key], n);
+            return rv;
+        },
+        {}
+    )
 }
-MyArray.prototype.get = function(i) {
-    return this.accArray[i]? this.accArray[i] : this.value;
-}
-MyArray.prototype.set = function(i,v) {
-    this.accArray[i] = v;
-}
-MyArray.prototype.setValue = function (v) {
-    this.value = v;
-    this.accArray = this.accArray.map(n => n = v);
-}
-const myArray1 = new MyArray(10);
-console.log(myArray1.get(100));
-myArray1.set(100, 500)//sets 500 at index 100
-console.log(myArray1.get(200)) //displayed out 10
-console.log(myArray1.get(100)) //displayed out 500
-myArray1.setValue(300);
-console.log(myArray1.get(100)) //displayed out 300
-console.log(myArray1.get(200)) //displayed out 300
 
+let r=groupBy(persons, p=>p.address.city, ()=>0, (rv,p)=>++rv);
+console.log(r);
+
+r = groupBy(persons, p=>p.address.city, () => [], (rv,p) => (rv.push(p.name), rv));
+console.log(r);
+// // -------------------------------
+// Write a functinon groupBy(array, .......), which has 
+// some callback parameters and performs the following actions
+// 1) breaks the array elements into groups by specified criteria
+// 2) for each group performs specified reduction operation
+// The same function would be able to show the following information 
+// when called with different callback parameters:
+// Example 1: { Rehovot: 2, 'Tel-Aviv': 1, Lod: 1 }
+// Example 2: { 
+//    Rehovot: [ 'Vasya', 'Olya' ],
+//    'Tel-Aviv': [ 'Tolya' ],
+//    Lod: [ 'Sara' ]
+// }
+//
+// Your tasks are:
+// - Decide which parameters must have such function
+// - Implement the function
+// - Implement the calls of this function implementing results of Example 1 and Example 2
+//------------------------------------
