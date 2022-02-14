4cb48e4b61cdfc984d1ecf61cf7477212598b5e8
Anton Balter <antbalter@gmail.com>
2022-02-14 18:23:32 +0200

    HW#19 solution

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 function getRandomNumber(min, max) {
-       let res = min > max? 
-       ([min, max] = [max, min]) && Math.floor(Math.random() * (max - min + 1) ) + min : 
-       Math.floor(Math.random() * (max - min + 1) ) + min;  
-       return res;
 //TODO
-//min -minimal value
+//min - minimal value
 //max - max value
 //Example getRandomNumber(0, 1) -> returns number that is either 0 or 1
 //if min > max then you should swap values
 //swap should be without additional variable in one line code
+if (min > max) {
+        [max, min] = [min, max];
 }
-res = getRandomNumber(999, 0);
-console.log(res);
+return   Math.round( min + Math.random() * (max - min));
+}
+for (let i = 0; i < 10; i++) {
+        console.log(getRandomNumber(13,10))
+}
 
 function concatinate(prefix) {
-        const res = '' + prefix;
-        return function (m) {return res + m};
-//TODO
-// that by using the following code
-// const concatApp = concatinate('App - ');
-// const concatMessage = concatApp('Test status: Done');
-// console.log(concatMessage);
-// there should be displayed out App - Test status: Done;
+        //TODO
+        //  that by using the following code
+//   const concatApp = concatinate('App - ');
+//    const concatMessage = concatApp('Test status: Done');
+//     console.log(concatMessage); 
+//     there should be displayed out App - Test status: Done
+return (...args) => [prefix, ...args].join('')
 }
 const concatApp = concatinate('App - ');
-const concatMessage = concatApp('Test status: Done');
-console.log(concatMessage);
+const concatMessage = concatApp('Test status: Done', "  good", ' very');
+console.log(concatMessage)
+
+
+function getRandomNumber(min, max) {
+        //TODO
+        //min - minimal value
+        //max - max value
+        //Example getRandomNumber(0, 1) -> returns number that is either 0 or 1
+        //if min > max then you should swap values
+        //swap should be without additional variable in one line code
+        if (min > max) {
+                [max, min] = [min, max];
+        }
+        return   Math.round( min + Math.random() * (max - min));
+        }
+        for (let i = 0; i < 10; i++) {
+                console.log(getRandomNumber(13,10))
+        }
+        
+function concatinate(prefix) {
+        //TODO
+        //  that by using the following code
+        //   const concatApp = concatinate('App - ');
+        //    const concatMessage = concatApp('Test status: Done');
+        //     console.log(concatMessage); 
+        //     there should be displayed out App - Test status: Done
+        return (...args) => [prefix, ...args].join('')
+}
+        const concatApp = concatinate('App - ');
+        const concatMessage = concatApp('Test status: Done', "  good", ' very');
+        console.log(concatMessage)
+        
+        
+        
